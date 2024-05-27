cbe0356b038399ddebb18ff1e1a9f2ab00bbcf14
Arbiona <arbiona.shahu@gmail.com>
2024-05-27 12:21:42 +0200

    LogoPreview: Fix `padding` of bg & `rotation` of icon

--- src/components/LogoPreview.jsx
+++ src/components/LogoPreview.jsx
@@ -12,17 +12,27 @@ const LogoPreview = () => {
 		setStorageValue(storageData);
 	}, [updateStorage]);
 
-	const Icon = ({ name, color, size }) => {
+	const Icon = ({ name, color, size, rotate }) => {
 		const LucidIcon = icons[name];
 		if (!LucidIcon) {
 			return;
 		}
-		return <LucidIcon name={name} color={color} size={size} />;
+		return (
+			<LucidIcon
+				name={name}
+				color={color}
+				size={size}
+				style={{ transform: `rotate(${rotate}deg)` }}
+			/>
+		);
 	};
 
 	return (
 		<div className="flex items-center justify-center h-screen">
-			<div className="h-[400px] w-[400px] bg-gray-200 outline-dotted outline-gray-300">
+			<div
+				className="h-[400px] w-[400px] bg-gray-200 outline-dotted outline-gray-300"
+				style={{ padding: storageValue?.bgPadding }}
+			>
 				{/* render icon */}
 				<div
 					className="h-full w-full flex items-center justify-center"
@@ -35,6 +45,7 @@ const LogoPreview = () => {
 						name={storageValue?.iconName}
 						color={storageValue?.iconColor}
 						size={storageValue?.iconSize}
+						rotate={storageValue?.iconRotate}
 					/>
 				</div>
 			</div>
