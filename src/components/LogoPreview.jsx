bfcfebd67bb98439e723e02b9a0d32cd63c21b2c
Arbiona <arbiona.shahu@gmail.com>
2024-05-27 12:05:27 +0200

    Preview icon

--- src/components/LogoPreview.jsx
+++ src/components/LogoPreview.jsx
@@ -1,9 +1,9 @@
 import { UpdateStorageContext } from "@/context/UpdateStorageContext";
+import { icons } from "lucide-react";
 import { useContext, useEffect, useState } from "react";
 
 const LogoPreview = () => {
 	const [storageValue, setStorageValue] = useState();
-
 	const { updateStorage, setUpdateStorage } = useContext(UpdateStorageContext);
 
 	useEffect(() => {
@@ -12,17 +12,31 @@ const LogoPreview = () => {
 		setStorageValue(storageData);
 	}, [updateStorage]);
 
+	const Icon = ({ name, color, size }) => {
+		const LucidIcon = icons[name];
+		if (!LucidIcon) {
+			return;
+		}
+		return <LucidIcon name={name} color={color} size={size} />;
+	};
+
 	return (
 		<div className="flex items-center justify-center h-screen">
 			<div className="h-[400px] w-[400px] bg-gray-200 outline-dotted outline-gray-300">
 				{/* render icon */}
 				<div
-					className="h-full w-full"
+					className="h-full w-full flex items-center justify-center"
 					style={{
 						borderRadius: storageValue?.bgRounded,
-						backgroundColor: storageValue?.bgColor,
+						background: storageValue?.bgColor,
 					}}
-				></div>
+				>
+					<Icon
+						name={storageValue?.iconName}
+						color={storageValue?.iconColor}
+						size={storageValue?.iconSize}
+					/>
+				</div>
 			</div>
 		</div>
 	);
